d151c41efa0f28a5d0469f26aa6eb76059ff5cd5
Micah Mosley <micah.mosley@utexas.edu>
2020-06-29 00:43:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence_array=sentence.split(" ")
   
   sentence_array.map do |word|
-    puts word.reverse
+    word.reverse
   end
   puts sentence_array
   sentence_array.join(" ")
